8dc02c53d03642d2f1dad95dae0839ab24b2c9f8
renanhangai <hangai.renan@gmail.com>
2017-05-03 20:24:49 -0300

    0.2.0

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
 var rollup = require('rollup');
 var memory = require('rollup-plugin-memory');
 var applySourceMap = require('vinyl-sourcemaps-apply');
@@ -27,39 +29,61 @@ function rollupStream(options, bundleCb) {
 		}
 		return resolveJs(rootDir, id);
 	}
+
 	return through2.obj(function (file, enc, callback) {
 		var thisOptions = {};
+		if (_typeof(options.rollup) === 'object') Object.assign(thisOptions, options.rollup);
+
 		thisOptions.cache = cache;
 		thisOptions.entry = file.path;
 		thisOptions.plugins = [memory({ contents: file.contents.toString() })].concat(options.plugins || []).concat({ resolveId: resolveId });
 		thisOptions.external = options.external;
 
-		rollup.rollup(thisOptions).then(function (bundle) {
-			cache = bundle;
+		var rollupOptions = thisOptions;
+		if (typeof options.rollup === 'function') {
+			rollupOptions = options.rollup.call(null, rollupOptions, { options: options, file: file });
+			if (rollupOptions == null) rollupOptions = thisOptions;
+		}
 
-			if (bundleCb) bundleCb(bundle);
+		Promise.resolve(rollupOptions).then(function (rollupOptions) {
+			return rollup.rollup(rollupOptions).then(function (bundle) {
+				cache = bundle;
 
-			var generate = {
-				format: options.format || 'cjs',
-				exports: options.exports,
-				moduleId: options.moduleId,
-				moduleName: options.moduleName,
-				globals: options.globals
-			};
-			if (file.sourceMap) {
-				generate.sourceMap = true;
-				generate.sourceMapFile = file.path;
-			}
-			var result = bundle.generate(generate);
+				if (bundleCb) bundleCb(bundle);
 
-			var newFile = options.clone ? file.clone() : file;
-			newFile.contents = new Buffer(result.code);
-			if (newFile.sourceMap) {
-				result.map.file = newFile.sourceMap.file || result.map.file;
-				applySourceMap(newFile, result.map);
-			}
+				var generate = {
+					format: options.format || 'cjs',
+					exports: options.exports,
+					moduleId: options.moduleId,
+					moduleName: options.moduleName,
+					globals: options.globals
+				};
+				if (_typeof(options.generate) === 'object') Object.assign(generate, options.generate);
 
-			callback(null, newFile);
+				if (file.sourceMap) {
+					generate.sourceMap = true;
+					generate.sourceMapFile = file.path;
+				}
+
+				var generateOptions = generate;
+				if (typeof options.generate === 'function') {
+					generateOptions = options.generate.call(null, generateOptions, { file: file, rollup: rollupOptions, bundle: bundle, options: options });
+					if (generateOptions == null) generateOptions = generate;
+				}
+
+				return Promise.resolve(generateOptions).then(function (generateOptions) {
+					var result = bundle.generate(generateOptions);
+
+					var newFile = options.clone ? file.clone() : file;
+					newFile.contents = new Buffer(result.code);
+					if (newFile.sourceMap) {
+						result.map.file = newFile.sourceMap.file || result.map.file;
+						applySourceMap(newFile, result.map);
+					}
+
+					callback(null, newFile);
+				});
+			});
 		}).catch(function (err) {
 			callback(err || new Error());
 		});
